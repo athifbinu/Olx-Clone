a71e23a61db79a597aa0b862b56889ca5a5a572b
Athif binu <binuathif@gmail.com>
2023-01-05 21:32:54 +0530

    router dom add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Signup from './Pages/Signup'
 import './App.css';
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import Signup from './Components/Signup/Signup';
+
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Route path='/'>
+
+        <Home/>
+
+        </Route>
+      </Router>
+   
     </div>
   );
 }
