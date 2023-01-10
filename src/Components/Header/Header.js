0b2ff277d7e543a4a3359437eb359b2558290c5b
Athif binu <binuathif@gmail.com>
2023-01-10 21:13:43 +0530

    bug solve

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,20 @@
 import React,{useContext} from 'react';
+import { useHistory } from 'react-router-dom';
+
 import './Header.css';
 import OlxLogo from '../../assets/OlxLogo';
 import Search from '../../assets/Search';
 import Arrow from '../../assets/Arrow';
 import SellButton from '../../assets/SellButton';
 import SellButtonPlus from '../../assets/SellButtonPlus';
-import { AuthContext } from '../../store/Context';
+import { AuthContext, FirebaseContext } from '../../store/Context';
+
+
+
 function Header() {
   const {user} =useContext(AuthContext)
-     console.log(user)
+  const {firebase}=useContext(FirebaseContext)
+  const history=useHistory()
 
   return (
     <div className="headerParentDiv">
@@ -39,8 +45,12 @@ function Header() {
         <div className="loginPage">
           <span>{user ?`Welcome ${user.displayName}` : "Login"}</span>
           <hr />
+        
         </div>
-
+       { user && <span onClick={()=>{
+        firebase.auth().signOut();
+           history.push('/login')
+       }}>Logout</span>}
         <div className="sellMenu">
           <SellButton></SellButton>
           <div className="sellMenuContent">
